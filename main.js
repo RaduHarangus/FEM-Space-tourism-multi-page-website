5eb5235aad6746ae5d127210a39615631ee13183
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-03-15 14:04:00 +0200

    7. navigation - menu button icon change and animation

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// const menuButton = document.getElementsByClassName('mobile-nav-toggle')[0];
-// const primaryNav = document.getElementsByClassName('primary-navigation')[0];
-// const ariaExpanded = menuButton.getElementsByTagName("span")[0];
-//
-// menuButton.addEventListener('click', function() {
-//     const ariaExpandedVal = ariaExpanded.getAttribute("aria-expanded") === 'true';
-//     ariaExpanded.setAttribute("aria-expanded", !ariaExpandedVal);
-//     primaryNav.style.transform = ariaExpandedVal ? "translateX(100%)" : "translateX(0)";
-//     menuButton.style.backgroundImage = ariaExpandedVal ? 'url("./assets/shared/icon-hamburger.svg")' : 'url("./assets/shared/icon-close.svg")';
-// });
-
 const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
@@ -16,9 +5,9 @@ navToggle.addEventListener("click", () => {
     const visibility = nav.getAttribute("data-visible");
     if (visibility === "false") {
         nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     } else {
         nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
-    navToggle.style.backgroundImage = visibility === "true" ? 'url("./assets/shared/icon-hamburger.svg")' : 'url("./assets/shared/icon-close.svg")';
-
 });
