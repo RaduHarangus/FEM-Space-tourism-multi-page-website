73bd7b0b49bf104efa963747039a5c27a0a99ee2
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-03-18 10:34:51 +0200

    tabs - keyboard navigation part 1

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,3 @@
-const nav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-
-navToggle.addEventListener("click", () => {
-    const visibility = nav.getAttribute("data-visible");
-    if (visibility === "false") {
-        nav.setAttribute("data-visible", true);
-        navToggle.setAttribute("aria-expanded", true);
-    } else {
-        nav.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false);
-    }
-});
-
 const tabList = document.querySelector('[role="tabList"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
