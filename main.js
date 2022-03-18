47aea609da1bfd16d5bc6cf740323d2c34d803c0
Radu H <52208792+Radoogle@users.noreply.github.com>
2022-03-18 10:53:03 +0200

    tabs - keyboard navigation part 2

--- main.js
+++ main.js
@@ -15,21 +15,22 @@ navToggle.addEventListener("click", () => {
 const tabList = document.querySelector('[role="tabList"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-function checkIndexTab(tab) {
-    return tab.getAttribute('tabindex') === '0';
-}
+let tabFocus = 0;
 
 tabList.addEventListener('keydown', (e) => {
     const keydownLeft = 37;
     const keydownRight = 39;
 
-    // change the tabindex of the current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
 
-    // console.log(tabs[0].getAttribute('tabindex'));
-    const currentTabIndex = tabs.findIndex(checkIndexTab);
-    tabs[currentTabIndex].setAttribute('tabindex', '-1');
+    if (e.keyCode === keydownRight) {
+        tabs[++tabFocus].setAttribute("tabindex", 0);
+    }
 
-    // if the right key is pushed, move to the next tab on the right
 
-    // if the left key is pushed, move to the next tab on the left
+    if (e.keyCode === keydownLeft) {
+        tabs[--tabFocus].setAttribute("tabindex", 0);
+    }
 });
